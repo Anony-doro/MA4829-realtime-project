4560702a8e5de3c7d6d05d90cb56f68ac0ffbf56
Anony-doro <medddbgm@gmail.com>
2025-04-23 16:09:27 +0800

    new start

---
+++ handmade code.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <signal.h>
+#include <hw/pci.h>
+#include <hw/inout.h>
+#include <sys/neutrino.h>
+#include <sys/mman.h>
+#include <math.h>
+#include <pthread.h>
+#include <time.h>
+
+#define	INTERRUPT		iobase[1] + 0				// Badr1 + 0 : also ADC register
+#define	MUXCHAN			iobase[1] + 2				// Badr1 + 2
+#define	TRIGGER			iobase[1] + 4				// Badr1 + 4
+#define	AUTOCAL			iobase[1] + 6				// Badr1 + 6
+#define DA_CTLREG		iobase[1] + 8				// Badr1 + 8
+
+#define	AD_DATA			iobase[2] + 0				// Badr2 + 0
+#define	AD_FIFOCLR		iobase[2] + 2				// Badr2 + 2
+
+#define	TIMER0			iobase[3] + 0				// Badr3 + 0
+#define	TIMER1			iobase[3] + 1				// Badr3 + 1
+#define	TIMER2			iobase[3] + 2				// Badr3 + 2
+#define	COUNTCTL		iobase[3] + 3				// Badr3 + 3
+#define	DIO_PORTA		iobase[3] + 4				// Badr3 + 4
+#define	DIO_PORTB		iobase[3] + 5				// Badr3 + 5
+#define	DIO_PORTC		iobase[3] + 6				// Badr3 + 6
+#define	DIO_CTLREG		iobase[3] + 7				// Badr3 + 7
+#define	PACER1			iobase[3] + 8				// Badr3 + 8
+#define	PACER2			iobase[3] + 9				// Badr3 + 9
+#define	PACER3			iobase[3] + a				// Badr3 + a
+#define	PACERCTL		iobase[3] + b				// Badr3 + b
+
+#define DA_Data			iobase[4] + 0				// Badr4 + 0
+#define	DA_FIFOCLR		iobase[4] + 2				// Badr4 + 2
+	
+int badr[5];															// PCI 2.2 assigns 6 IO base addresses
+
+struct pci_dev_info info;
+void *hdl;
+
+uintptr_t dio_in;
+uintptr_t iobase[6];
+uint16_t adc_in;
+
+unsigned int i, count;
+unsigned short chan;
+unsigned int data[100];
+float delta, dummy;
+
+
+void* check_input(){
+    while(1){
+        pthread_mutex_lock(&mutex);
+        pthread_cond_wait(&cond
+    }
+}
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+int main(){
+    printf("\fWelcome to the Waveform Generator\n\n");
+
+    memset(&info, 0, sizeof(info)); // clear the info structure
+    if (pci_attach(0)<0){
+        perror("Failed to attach to PCI device");
+        exit(EXIT_FAILURE);
+    }
+
+    // Vendor and Device ID
+    info.Vendor_id = 0x1307;
+    info.Device_id = 0x01;
+    
+    if ((hdl=pci_attach_device(0, PCI_SHARE|PCI_INIT_ALL, 0, &info)) == 0){
+        perror("Failed to attach to PCI device");
+        exit(EXIT_FAILURE);
+    }
+
+    // Determine assigned BADRn IO addresses for PCI-DAS1602
+    for (i=0; i<5; i++){
+        badr[i] = PCI_IO_ADDR(info.CpuBaseAddress[i]);
+    }
+
+    printf("\nReconfirm Iobase:\n");      //map I/O base address to user space
+    for (i=0; i<5; i++){                    // expect CpuBaseAddress to be the same as iobase for PC
+        iobase[i] = mmap_device_io(0x0f, badr[i]);
+        printf("Index %d : Address : %x\n", i, badr[i]);
+        printf("IOBASE : %x\n", iobase[i]);
+    }
+
+    // Modify thread control privity
+    if (ThreadCtl(_NTO_TCTL_IO, 0) == -1){
+        perror("Failed to modify thread control privity");
+        exit(1);
+    }
+
+        //***************************************** */
+        //D/A Port Functions
+        //***************************************** */
+
+    pthread_create(NULL, NULL, &check_input, NULL); //later change check_input to check_input()
+    pthread_create(NULL, NULL, &generate_data, NULL); //later change generate_data to generate_data()
+    return generate_wave(); //later change generate_wave to generate_wave()
+}
+`
